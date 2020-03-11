9cc2fa5f374cd1abc137351d4e93b9993c54b6d7
Mark Grebe <grebe@ucmo.edu>
2020-03-10 22:11:03 -0500

    Simplified User model

--- src/main/java/edu/ucmo/spring_example/model/User.java
+++ src/main/java/edu/ucmo/spring_example/model/User.java
@@ -1,10 +1,6 @@
 package edu.ucmo.spring_example.model;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import java.util.Set;
 
 @Entity
@@ -15,26 +11,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id")
     private Integer id;
+
     @Column(name = "user_name")
-    @Length(min = 5, message = "*Your user name must have at least 5 characters")
-    @NotEmpty(message = "*Please provide a user name")
     private String userName;
+
     @Column(name = "email")
-    @Email(message = "*Please provide a valid Email")
-    @NotEmpty(message = "*Please provide an email")
     private String email;
+
     @Column(name = "password")
-    @Length(min = 5, message = "*Your password must have at least 5 characters")
-    @NotEmpty(message = "*Please provide your password")
-    private String password;
+     private String password;
+
     @Column(name = "first_name")
-    @NotEmpty(message = "*Please provide your name")
     private String firstName;
+
     @Column(name = "last_name")
-    @NotEmpty(message = "*Please provide your last name")
     private String lastName;
+
     @Column(name = "active")
     private Boolean active;
+
     @ManyToMany(cascade = CascadeType.MERGE)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
