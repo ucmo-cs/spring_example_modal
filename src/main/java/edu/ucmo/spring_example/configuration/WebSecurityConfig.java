7609d91aa162b27731b2f9e86b18b2a453124210
Mark Grebe <grebe@ucmo.edu>
2020-03-10 21:57:58 -0500

    Small security fixes

--- src/main/java/edu/ucmo/spring_example/configuration/WebSecurityConfig.java
+++ src/main/java/edu/ucmo/spring_example/configuration/WebSecurityConfig.java
@@ -2,16 +2,12 @@ package edu.ucmo.spring_example.configuration;
 
 import edu.ucmo.spring_example.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
--- src/main/java/edu/ucmo/spring_example/controller/LoginController.java
+++ src/main/java/edu/ucmo/spring_example/controller/LoginController.java
@@ -1,12 +1,12 @@
 package edu.ucmo.spring_example.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login")
+    @GetMapping(value={"/", "/login"})
     public String index() {
         return "login";
     }
