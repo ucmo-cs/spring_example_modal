2b223446808a3de8d24779b93aef54aae2ceae29
Mark Grebe <grebe@ucmo.edu>
2020-03-13 21:54:12 -0500

    Heroku Deploy Adddress

--- src/main/js/services/ApiService.js
+++ src/main/js/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const CAR_API_BASE_URL = 'http://localhost:8080/cars';
+const CAR_API_BASE_URL = 'https://grebe-spring-example.herokuapp.com/cars';
 
 class ApiService {
 
