fd96fa4aa769f9f6f66813962e7cfa457f1e7fd9
Mark Grebe <grebe@ucmo.edu>
2020-03-14 21:38:35 -0500

    Update schema for PostgreSQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS car;
 CREATE TABLE car (
-  id     INT         NOT NULL AUTO_INCREMENT,
+  id     SERIAL PRIMARY KEY,
   make   VARCHAR(50) NOT NULL,
   model  VARCHAR(50) NOT NULL,
   year   INT         NOT NULL,
