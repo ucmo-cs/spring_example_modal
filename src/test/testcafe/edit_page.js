77d881324d7f3142ae60789b31a11507270fc182
Mark Grebe <grebe@ucmo.edu>
2024-01-07 21:46:30 -0600

    Update edit_page.js

--- src/test/testcafe/edit_page.js
+++ src/test/testcafe/edit_page.js
@@ -35,7 +35,7 @@ test('Add Test', async t => {
 
 test('Delete Test', async t => {
     await t
-        .click(Selector('button').withText('Delete').nth(-1).nth(0))
+        .click(Selector('button').withText('Delete').nth(-1))
         .expect(Selector('tr').nth(-1).child().nextSibling(0).innerText).notEql('Pontiac')
         .expect(Selector('tr').nth(-1).child().nextSibling(1).innerText).notEql('TransAm')
         .expect(Selector('tr').nth(-1).child().nextSibling(2).innerText).notEql('1993');
